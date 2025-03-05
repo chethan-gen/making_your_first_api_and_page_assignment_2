7a39012347eb32dc4cf6fca95811681fb379a17e
chethan-gen <chaithuregala123@gmailcom>
2025-03-05 12:05:44 +0530

    adding files

--- server.js
+++ server.js
@@ -50,6 +50,40 @@ List of Status Codes to Handle:
 200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
 */
 
+const statusCode = [
+  {code: 200, message:  "OK: The request has succeeded. The meaning of this status depends on the HTTP method used."},
+  {code:201, message:  "created:The request has been fulfilled and a new resourse has been created"},
+  {code:204, message:  "No content:The server successfully processed the request and not returning any content"},
+  {code:400, message: "Bad request:The server is not able to process the request due to client side errors(e.g. malformed syntax)"},
+  {code:401, message: "Unauthorized: The request has not been applied because it lacks valid authentication credentials for the target resource"},
+  {code:403, message: "Forbidden: The server understood the request but refuses to authoriz it to be fulfilled"},
+  {code:404, message: "Not Found: The server has not found anything matching the request URI. This is often caused by a missing page or resource." },
+  {code:405, message: "Method not allowed: The method is known by the server but is not supported by the target resourc"},
+  {code:429, message: "Too many request: The user has sent too many request in a given amount of time"},
+  {code:500, message: "Internal server error: The server encountered an unexpected condition that prevented it from fulfilling the request"},
+  {code:502, message: "Bad gateway: The server received an invalid response from the upstream server"},
+  {code:503, message: "Service unavailable: The server is currently unable to handle the request due to a temporary overload or scheduled maintenance"},
+  {code:504, message: "Gateway timeout: The server, while acting as a gateway or proxy, did not receive a timely response from an upstream server it needed to access in order to complete the request."}
+  
+]
+app.get('/status-info',(req,res)=>{
+  const code = parseInt(req.query.code);
+
+  const statusInfo = statusCode.find(item=>item.status===code);
+
+  if(statusInfo){
+    res.json(statusInfo);
+  }else{
+    res.status(400).json({
+      status: 400,
+      message: 'Bad request:The server is not able to process the request due to client side errors(e.g. malformed syntax)'
+    });
+  }
+})
+
+
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Status Code API is running on http://localhost:${PORT}`);
